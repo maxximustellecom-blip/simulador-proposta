2090247d57fd8e5fa72e0911266d9ce75993f369
Tekhno Develop <tekhnosoftdevelop@gmail.com>
2025-12-16 01:56:33 -0300

    Update clientController.js

--- backend/controllers/clientController.js
+++ backend/controllers/clientController.js
@@ -14,7 +14,7 @@ export async function upsertClient(req, res) {
     }
     const actor = req.user || null;
     const [client] = await Client.findOrCreate({
-      where: { },
+      where: { cnpj },
       defaults: {
         name, cnpj,
         created_by: actor && actor.id ? Number(actor.id) : null,
