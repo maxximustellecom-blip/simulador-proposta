a3868f806847cd2edc92ac5a43c6ad593736cb5e
Tekhno Develop <tekhnosoftdevelop@gmail.com>
2025-12-22 01:18:06 -0300

    updates

--- backend/models/Category.js
+++ backend/models/Category.js
@@ -5,7 +5,7 @@ const Category = sequelize.define('Category', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   nome: { type: DataTypes.STRING(160), allowNull: false },
   descricao: { type: DataTypes.STRING(255), allowNull: true },
-  tipo: { type: DataTypes.ENUM('Novo', 'Adtivo', 'Renegociação'), allowNull: false, defaultValue: 'Novo' }
+  tipo: { type: DataTypes.STRING(255), allowNull: false, defaultValue: 'Novo' }
 }, {
   tableName: 'categories',
   timestamps: true,
--- backend/models/CustomCategory.js
+++ backend/models/CustomCategory.js
@@ -5,7 +5,7 @@ const CustomCategory = sequelize.define('CustomCategory', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   nome: { type: DataTypes.STRING(160), allowNull: false },
   descricao: { type: DataTypes.STRING(255), allowNull: true },
-  tipo: { type: DataTypes.ENUM('Novo', 'Adtivo', 'Renegociação'), allowNull: false, defaultValue: 'Novo' }
+  tipo: { type: DataTypes.STRING(255), allowNull: false, defaultValue: 'Novo' }
 }, {
   tableName: 'custom_categories',
   timestamps: true,
